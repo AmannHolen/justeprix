59f9c7b5c990dad52d6fbc56f190523ffa2f9bde
AmannHolen <louise.godec@gmail.com>
2026-08-21 14:48:51 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,12 @@
+[
+  { "nom": "Un kilo de courgette bio", "valeur": 3.60, "image": "" },
+  { "nom": "Une paire de chaussures de running HOKA Clifton 10", "valeur": 160, "image": "" },
+  { "nom": "Des friandises pour Douglas (pommes séchées 60g)", "valeur": 4.79, "image": "images/lapin_chips_pomme.jpg" },
+  { "nom": "Une bouteille de Champagne Mumm", "valeur": 29.90, "image": "" },
+  { "nom": "Un billet aller-retour Paris-Cuba en septembre (AirFrance)", "valeur": 793, "image": "" },
+  { "nom": "Un scooter électrique", "valeur": 1990, "image": "" },
+  { "nom": "Un billig Krampouz 33cm", "valeur": 289, "image": "" },
+  { "nom": "Le prix au kilo d'un homard bleu en criée en aout", "valeur": 39, "image": "" },
+  { "nom": "Une poule rousse", "valeur": 15, "image": "" },  
+  { "nom": "Un billet concert pour Matmatah en fosse à Brest", "valeur": 36, "image": "" }
+]
